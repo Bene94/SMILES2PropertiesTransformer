666ce22001f0bd46d8d56ab2abfa46bf3514ae5d
Benedikt Winter <benedikt.winterq@rwth-aachen.de>
2021-09-04 01:22:51 -0700

    fix

--- minGPT.py
+++ minGPT.py
@@ -69,7 +69,7 @@ class Block(nn.Module):
         self.ln2 = nn.LayerNorm(config.embed_size)
         self.attn = CausalSelfAttention(config)
         self.mlp = nn.Sequential(
-            nn.Linear(config.embed_size, config.hid_fac * config.embed_size),
+            nn.Linear(config.embed_size, config.hidden_factor * config.embed_size),
             nn.GELU(),
             nn.Linear(4 * config.embed_size, config.embed_size),
             nn.Dropout(config.dropout),
