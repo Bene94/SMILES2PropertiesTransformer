bfaf56d2291b3619143e29aec328186bf7f126e2
Benedikt Winter <benedikt.winterq@rwth-aachen.de>
2021-11-21 04:18:51 -0800

    wip

--- fine_tune_mult.py
+++ fine_tune_mult.py
@@ -133,18 +133,15 @@ def main(model_name, data_path, batch_size, epochs, lr, weight_decay, cuda, mult
             # evaluate the 3 validation sets
 
         
-        temp_val_loss, temp_val_prediction, temp_val_target = evaluate(model, val_0_data, criterion, config)
-
+        temp_val_loss, temp_val_prediction, temp_val_target, __ = evaluate(model, val_0_data, criterion, config)
         val_predction_0 = np.concatenate((val_predction_0, temp_val_prediction), axis=0)
         val_target_0 = np.concatenate((val_target_0, temp_val_target), axis=0)
 
-        temp_val_loss, temp_val_prediction, temp_val_target = evaluate(model, val_1_data, criterion, config)
-
+        temp_val_loss, temp_val_prediction, temp_val_target, __  = evaluate(model, val_1_data, criterion, config)
         val_predction_1 = np.concatenate((val_predction_1, temp_val_prediction), axis=0)
         val_target_1 = np.concatenate((val_target_1, temp_val_target), axis=0)
 
-        temp_val_loss, temp_val_prediction, temp_val_target = evaluate(model, val_2_data, criterion, config)
-
+        temp_val_loss, temp_val_prediction, temp_val_target, __  = evaluate(model, val_2_data, criterion, config)
         val_predction_2 = np.concatenate((val_predction_2, temp_val_prediction), axis=0)
         val_target_2 = np.concatenate((val_target_2, temp_val_target), axis=0)
     
