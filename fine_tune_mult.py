1c15f2dc3617b36f2ebeb0517d96c2b1e48c30d0
Benedikt Winter <benedikt.winterq@rwth-aachen.de>
2021-11-22 00:36:06 -0800

    added loogging of val to fine tuen

--- fine_tune_mult.py
+++ fine_tune_mult.py
@@ -75,7 +75,7 @@ def main(model_name, data_path, batch_size, epochs, lr, weight_decay, cuda, mult
     config.local = local
     config.xp_name = xp_name
 
-    wandb.init(project='GNN_001_FT', entity='bene94', name=name, config=config)
+    wandb.init(project='GNN_001_FT_mult', entity='bene94', name=name, config=config)
     wandb.watch(model)
 
     print("=" * 50)
@@ -136,17 +136,17 @@ def main(model_name, data_path, batch_size, epochs, lr, weight_decay, cuda, mult
         temp_val_loss, temp_val_prediction, temp_val_target, __ = evaluate(model, val_0_data, criterion, config)
         val_predction_0 = np.concatenate((val_predction_0, temp_val_prediction), axis=0)
         val_target_0 = np.concatenate((val_target_0, temp_val_target), axis=0)
-        wandb.log({"val_0": torch.mean(temp_val_loss)})
+        wandb.log({"val_0_ft": torch.mean(temp_val_loss)})
 
         temp_val_loss, temp_val_prediction, temp_val_target, __  = evaluate(model, val_1_data, criterion, config)
         val_predction_1 = np.concatenate((val_predction_1, temp_val_prediction), axis=0)
         val_target_1 = np.concatenate((val_target_1, temp_val_target), axis=0)
-        wandb.log({"val_1": torch.mean(temp_val_loss)})
+        wandb.log({"val_1_ft": torch.mean(temp_val_loss)})
 
         temp_val_loss, temp_val_prediction, temp_val_target, __  = evaluate(model, val_2_data, criterion, config)
         val_predction_2 = np.concatenate((val_predction_2, temp_val_prediction), axis=0)
         val_target_2 = np.concatenate((val_target_2, temp_val_target), axis=0)
-        wandb.log({"val_2": torch.mean(temp_val_loss)})
+        wandb.log({"val_2_ft": torch.mean(temp_val_loss)})
     
     np.save(path_temp + 'val_prediction_array_0_' + xp_name + '.npy', val_predction_0)
     np.save(path_temp + 'val_target_array_0_' + xp_name + '.npy', val_target_0)
