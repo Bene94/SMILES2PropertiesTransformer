1bb5b9574c1189dc62ea0d1bca259236a26f661e
Benedikt Winter <benedikt.winterq@rwth-aachen.de>
2021-12-30 11:43:23 -0800

    added rights changer

--- change_rights.py
+++ change_rights.py
@@ -1,7 +1,12 @@
 import os  
+import shutil
 path = "/mnt/xprun"
-for root, dirs, files in os.walk(path):  
-  for momo in dirs:  
-    os.chown(os.path.join(root, momo), 1001, 1001)
-  for momo in files:
-    os.chown(os.path.join(root, momo), 1001, 1001)
+#path = "/home/bene/NNGamma"
+
+def recursive_chown(path, owner, group):
+    for dirpath, dirnames, filenames in os.walk(path):
+        shutil.chown(dirpath, owner, group)
+        for filename in filenames:
+            shutil.chown(os.path.join(dirpath, filename), owner, group)
+
+recursive_chown(path, 1001, 1001)
